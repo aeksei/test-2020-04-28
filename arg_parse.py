88a262698e44981b4f4d87277e69f0433ef5d8d7
aeksei <aeksei94@gmail.com>
2020-04-28 21:12:55 +0300

    удален лишний закоменировый код

--- arg_parse.py
+++ arg_parse.py
@@ -1,13 +1,6 @@
 import sys
 import argparse
 
-# print(sys.argv)
-# if len(sys.argv) < 2:
-#     print("Аргументов нет")
-# elif len(sys.argv) > 3:
-#     print("Аргументов много")
-# else:
-#     print(f"Hello {sys.argv[1]}. Count: {sys.argv[2]}")
 
 
 def create_parser():
@@ -26,12 +19,6 @@ if __name__ == "__main__":
 
     print(namespace)  # в переменной namespace будут находиться ваши расперсеные аргументы
 
-    # if namespace.file_list:
-    #     for file in namespace.file_list:
-    #         for line in file:
-    #             print(line, end="")
-    # else:
-    #     print("Файлов нет")
 
 
 
